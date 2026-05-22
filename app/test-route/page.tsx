20082230bab406773977da0abc458e9bf8975526
Fei Rui <ruifeiqiye@126.com>
2026-05-22 19:55:57 +0800

    test: add test route page

---
+++ app/test-route/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Test Route - Working!</h1>
+      <p>If you can see this, new routes are deploying correctly.</p>
+    </div>
+  )
+}
